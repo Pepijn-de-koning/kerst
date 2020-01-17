a72aaea32e2cf99f363f28a3bd4a2f255092c003
Pepijn-de-koning <54800145+Pepijn-de-koning@users.noreply.github.com>
2020-01-17 13:27:27 +0100

    Add files via upload

---
+++ kerstpret/script.js
@@ -0,0 +1,39 @@
+let kerstplaatjes = [
+	'<img src="https://images.pexels.com/photos/1708601/pexels-photo-1708601.jpeg?cs=srgb&dl=lighted-christmas-tree-1708601.jpg&fm=jpg" alt="leuk kerstplaatje afb1" id="afb1">',
+	'<img src="https://images.pexels.com/photos/754430/pexels-photo-754430.jpeg?cs=srgb&dl=turned-on-red-and-blue-merry-christmas-neon-sign-754430.jpg&fm=jpg" alt="leuk kerstplaatje afb2" id="afb2">',
+	'<img src="https://images.pexels.com/photos/1028724/pexels-photo-1028724.jpeg?cs=srgb&dl=snowman-and-drum-decor-1028724.jpg&fm=jpg" alt="leuk kerstplaatje afb3" id="afb3">',
+	'<img src="https://images.pexels.com/photos/1656564/pexels-photo-1656564.jpeg" alt="leuk kerstplaatje afb4" id="afb4">',
+	'<img src="https://images.pexels.com/photos/278508/pexels-photo-278508.jpeg" alt="leuk kerstplaatje afb5" id="afb5">',
+	'<img src="https://images.pexels.com/photos/246576/pexels-photo-246576.jpeg?cs=srgb&dl=close-up-of-baby-girl-246576.jpg&fm=jpg" alt="leuk kerstplaatje afb6" id="afb6">',
+	'<img src="https://images.pexels.com/photos/1596578/pexels-photo-1596578.jpeg?cs=srgb&dl=close-up-photo-of-bull-dog-near-ornaments-1596578.jpg&fm=jpg" alt="leuk kerstplaatje afb7" id="afb7">',
+	'<img src="https://images.pexels.com/photos/1669072/pexels-photo-1669072.jpeg?cs=srgb&dl=photo-of-lighted-christmas-tree-at-night-1669072.jpg&fm=jpg" alt="leuk kerstplaatje afb8" id="afb8">',
+	'<img src="https://images.pexels.com/photos/6270/woman-flowers-holidays-girl-6270.jpg" alt="leuk kerstplaatje afb9" id="afb9">',
+	'<img src="https://images.pexels.com/photos/735423/pexels-photo-735423.jpeg" alt="leuk kerstplaatje afb10" id="afb10">'
+
+];
+
+var item4 = document.getElementById('item4');
+var item5 = document.getElementById('item5');
+var item6 = document.getElementById('item6');
+var item7 = document.getElementById('item7');
+var item8 = document.getElementById('item8');
+var item9 = document.getElementById('item9');
+var item10 = document.getElementById('item10');
+var item11 = document.getElementById('item11');
+var item12 = document.getElementById('item12');
+var item13 = document.getElementById('item13');
+
+item4.innerHTML = kerstplaatjes[0];
+item5.innerHTML = kerstplaatjes[1];
+item6.innerHTML = kerstplaatjes[2];
+item7.innerHTML = kerstplaatjes[3];
+item8.innerHTML = kerstplaatjes[4];
+item9.innerHTML = kerstplaatjes[5];
+item10.innerHTML = kerstplaatjes[6];
+item11.innerHTML = kerstplaatjes[7];
+item12.innerHTML = kerstplaatjes[8];
+item13.innerHTML = kerstplaatjes[9];
+
+
+// theo heeft dit gezien op 13 jan
+// daar werkt het
